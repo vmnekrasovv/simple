829dc2d95c0fde577c5adeefacee2c4f15e24a30
test <test@gmail.com>
2024-05-07 21:59:02 +0300

    init

---
+++ app/js/app.js
@@ -0,0 +1,29 @@
+(function($){
+	$(document).ready(function(){ 
+
+		/*$('.scroll-down').on('click', function(){
+			$('html, body').animate({
+				scrollTop: $('.navbar').offset().top
+			}, 500, function(){ 
+				//console.log('callback function'); 
+			});
+		});
+
+		$(window).on('scroll load', function(e){
+			if(pageYOffset > $('.header').outerHeight(true)){
+				$('.navbar').addClass('fixed');
+				$('body').addClass('navbar-fixed');
+			} else {
+				$('.navbar').removeClass('fixed');
+				$('body').removeClass('navbar-fixed');
+			}
+		});
+
+
+		$('.header').parallax({
+			imageSrc: 'images/dest/header_bg.jpg',
+			speed: .7,
+		});*/
+		
+	});
+})(jQuery);
